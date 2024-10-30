5ddabdc11724d718b3f235519c16a979f4b45b02
riaacordero <riarcordero@gmail.com>
2024-10-30 21:55:28 +0800

    adjusted video duration to adapt to audio duration

--- scripts/video.py
+++ scripts/video.py
@@ -1,7 +1,7 @@
 from moviepy.editor import VideoFileClip, AudioFileClip, CompositeVideoClip
 
 # MODIFY YOUR FILE PATH IF NEEDED:
-video_path = "./data/input_video/video3.mp4"
+video_path = "./data/input_video/video2.mp4"
 audio_path = "./output/audio/narration.mp3"
 output_video_path = "./output/video/new_video.mp4"
 
@@ -9,9 +9,12 @@ output_video_path = "./output/video/new_video.mp4"
 audio_clip = AudioFileClip(audio_path)
 video_clip = VideoFileClip(video_path)
 
-# Set the audio for the video
-video_clip = video_clip.set_audio(audio_clip)
+# Trim the video to match the audio length
+audio_duration = audio_clip.duration
+video_clip = video_clip.subclip(0, audio_duration)
 
-video_clip.write_videofile(output_video_path, fps=24)
+video = video_clip.set_audio(audio_clip)
+
+video.write_videofile(output_video_path, fps=24)
 
 print("Video created successfully!")
