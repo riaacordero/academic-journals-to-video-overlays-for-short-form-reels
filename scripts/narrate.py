8209165312fb93ac657180f4347ba12d22c9b7b6
riaacordero <riarcordero@gmail.com>
2024-10-30 21:14:45 +0800

    replaced pyttsx3 to gtts (former is not working)

--- scripts/narrate.py
+++ scripts/narrate.py
@@ -1,22 +1,21 @@
-import pyttsx3
 import os
+from gtts import gTTS
+from tqdm import tqdm
 
 def text_to_speech(text, output_dir="audio_files", filename="narration.mp3"):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    file_path = os.path.join(output_dir, filename)
 
-    engine = pyttsx3.init()
+    file_path = os.path.join(output_dir, filename)
     
-    engine.setProperty('rate', 150)  # Speed of speech
-    engine.setProperty('volume', 1)  # Volume (0.0 to 1.0)
+    sentences = text.split('. ')
     
-    engine.save_to_file(text, file_path)
-    engine.runAndWait()
-    
+    for sentence in tqdm(sentences, desc="Processing Sentences", unit="sentence"):
+        tts = gTTS(text=sentence, lang='en')
+        tts.save(file_path)
+
     print(f"Audio saved to: {file_path}")
 
-# Example usage
 if __name__ == "__main__":
     sample_text = "This is a test narration for the text-to-speech conversion."
     text_to_speech(sample_text)
